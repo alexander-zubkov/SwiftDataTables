d571bb3a187ac506a3325f4a280492a18eec8598
Alexander Zubkov <41280743+alexander-zubkov@users.noreply.github.com>
2021-08-13 16:10:40 +0500

    Update DataCell.swift
    added border lines (bottom and right)

--- SwiftDataTables/Classes/Cells/DataCell/DataCell.swift
+++ SwiftDataTables/Classes/Cells/DataCell/DataCell.swift
@@ -39,10 +39,19 @@ class DataCell: UICollectionViewCell {
             dataLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: Properties.horizontalMargin),
             dataLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: Properties.horizontalMargin),
         ])
+        
+        let bottomLine = CALayer()
+        bottomLine.frame = CGRect(x: 0, y: self.frame.size.height - 1, width: self.frame.size.width, height: 1)
+        bottomLine.backgroundColor = UIColor.gray.cgColor
+        contentView.layer.addSublayer(bottomLine)
+               
+        let bottomRight = CALayer()
+        bottomRight.frame = CGRect(x: self.frame.size.width - 1, y: 0, width: 1, height: self.frame.size.height)
+        bottomRight.backgroundColor = UIColor.gray.cgColor
+        contentView.layer.addSublayer(bottomRight)
     }
     
-    func configure(_ viewModel: DataCellViewModel){
+    func configure(_ viewModel: DataCellViewModel) {
         self.dataLabel.text = viewModel.data.stringRepresentation
-//        self.contentView.backgroundColor = .white
     }
 }
